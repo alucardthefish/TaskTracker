eb54ff885d7274dd72a80968d763e94e1b45c43f
alucardthefish <sergiop18@gmail.com>
2021-03-13 14:52:03 -0500

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+//import React from "react";
+import Header from "./components/Header";
 
 function App() {
   const name = 'Brad';
   return (
-    <div className="App">
-      <h1>Hello from react</h1>
-      <h2>Hello {name} </h2>
+    <div className="container">
+      <Header />
     </div>
   );
 }
+
+/* class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>
+  }
+} */
 
 export default App;
