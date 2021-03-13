8434e787c47fed909fe4bbf393997ac2e53df0bc
alucardthefish <sergiop18@gmail.com>
2021-03-13 16:33:48 -0500

    Add Button component with styling and event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        alert('Click')
+    }
+
     return (
-        <header>
+        <header className='header'>
             <h1> { title } </h1>
+            <Button color='green' text='Hello' onClick={onClick} />
         </header>
     )
 }
@@ -16,5 +22,10 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
